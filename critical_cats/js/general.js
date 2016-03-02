4ba172e50d994322ce32351ccbd59856b2d2a611
Keely Goulding <keely.goulding@uleth.ca>
2016-03-02 00:02:31 -0700

    update

--- critical_cats/js/general.js
+++ critical_cats/js/general.js
@@ -101,10 +101,10 @@ $('.slider').each(function(){
 
 //SPECIES NAV HOVER
 var show = function (e){
-  var x = "#";
-  x += e.target.id ;
-  x += " .hover";
-	$(x).show();
+	  var x = "#";
+	  x += e.target.id ;
+	  x += " .hover";
+		$(x).show();
 };
 
 var hide = function () {
@@ -112,5 +112,37 @@ var hide = function () {
 };
 
 $(".spec_nav").hover(show, hide);
+
+
+
+//FLICKR
+var flickr = function () {
+    "use strict";
+
+    var requestURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=wildcat,cat,endangered&format=json&jsoncallback=?";"https://api.flickr.com/services/feeds/groups_pool.gne?id=smallwildcats&format=json&jsoncallback=?";
+
+			$.getJSON(requestURL, function(flickrResponse) {
+						flickrResponse.items.forEach(function (item) {
+				
+							// create a new JQuery element to hold the image
+							// but hide it so we can fade it in
+							var $img = $("<img>").hide();
+				
+							// set the attribute to the url
+							// contained in the response
+							$img.attr("src", item.media.m);
+				
+				
+							// attach the img tag to the main
+							// photos element and then fade it in
+							$(".photos").append($img);
+							$img.fadeIn();
+						});
+		
+			});
+};
+
+$(document).ready(flickr);
+
 
 });
