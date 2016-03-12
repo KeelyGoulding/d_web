62023c745c2d5ca254be04fa40519c23334cd0dc
Keely Goulding <keely.goulding@uleth.ca>
2016-03-11 20:32:07 -0700

    ch4 simplified

--- chapters/chapter4/app.js
+++ chapters/chapter4/app.js
@@ -1,29 +1,26 @@
 var main = function(){
 	"use strict";
 	
-	$(".comment-input button").on("click", function (event){		
+	var addNewComment = function () {
 		var $new_comment
-		
+				
 		if ($(".comment-input input").val() !== "") {
-		var $new_comment = $("<p>").text($(".comment-input input").val());
-		$new_comment.hide();
-		$(".comments").append($new_comment);
-		$new_comment.fadeIn();
-		$(".comment-input input").val("");
-		}
-	});
-	
-	$(".comment-input input").on("keypress", function (event) {
-		var $new_comment
-		
-		if (event.keyCode === 13){
-			if ($(".comment-input input").val() !== "") {
 			var $new_comment = $("<p>").text($(".comment-input input").val());
 			$new_comment.hide();
 			$(".comments").append($new_comment);
 			$new_comment.fadeIn();
 			$(".comment-input input").val("");
-			}
+		}
+	}
+	
+	
+	$(".comment-input button").on("click", function (event){		
+		addNewComment();
+	});
+	
+	$(".comment-input input").on("keypress", function (event) {
+		if (event.keyCode === 13){
+			addNewComment();
 		}
 	});
 	
